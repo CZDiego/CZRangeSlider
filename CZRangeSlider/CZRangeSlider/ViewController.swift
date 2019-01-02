ea82e52b2ae4c42d488f3fad6ad739b4355986f1
Diego Contreras <czdiego.96@gmail.com>
2019-01-02 11:54:56 -0600

    CZRangeSlider Initial Commit

---
+++ CZRangeSlider/CZRangeSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CZRangeSlider
+//
+//  Created by Diego Contreras on 1/2/19.
+//  Copyright © 2019 Diego Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
